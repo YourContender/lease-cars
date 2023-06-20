3bb82e3c797921eca805e9826d6862e9a905858e
YourContender <dream-boy123@ukr.net>
2023-06-21 01:20:11 +0300

    init commit

---
+++ js/index.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper(".mySwiper", {
+  effect: 'fade',
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false
+  }
+});
