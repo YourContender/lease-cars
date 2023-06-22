0686071e1a8f48662ca7f8370be29351a6ff120d
YourContender <dream-boy123@ukr.net>
2023-06-22 03:46:36 +0300

    add tabs in app

--- js/index.js
+++ js/index.js
@@ -1,3 +1,20 @@
+const tabsItem = document.querySelectorAll('.tabs_btn-item');
+
+tabsItem.forEach(item => {
+  item.addEventListener('click', open);
+})
+
+function open(e) {
+  const target = e.currentTarget;
+
+  tabsItem.forEach(item => {
+    item.classList.remove('tabs_btn-item-active');
+  })
+
+  target.classList.add('tabs_btn-item-active');
+}
+
+ 
 const swiper = new Swiper(".mySwiper", {
   effect: 'fade',
   pagination: {
